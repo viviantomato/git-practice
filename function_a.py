fd3d9e8886b18e40ccba1381484a7bdca8583590
viviantomato <vivianyw924@gmail.com>
2022-09-22 14:23:41 -0700

    conflict3

--- function_a.py
+++ function_a.py
@@ -15,9 +15,9 @@ def most_common_value(number_list):
         else:
             frequency_index[num] = 1
 
-        if max_frequency > frequency_index[num]:
-            max_frequency = frequency_index[num]
-            most_common_value = num
+        #if max_frequency > frequency_index[num]:
+           #max_frequency = frequency_index[num]
+            #most_common_value = num
 
     #return most_common_value
 
